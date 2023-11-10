06a4301f43a4a6c51c5b83f46a2a72465fb274f8
ButongAnnie <ynzaballero.butong@gmail.com>
2023-11-10 15:12:58 +0800

    added constructor

--- src/inheritance/vegetables.java
+++ src/inheritance/vegetables.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package inheritance;
 
-/**
- *
- * @author Admin
- */
+
 public class vegetables {
+    
+    String utan;
+    public vegetables(String utan){
+        this.utan =utan;
+    }
+    
     
 }

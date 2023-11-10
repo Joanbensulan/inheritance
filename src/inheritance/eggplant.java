0043d2ae66ce0e286ed99647741fa7218c14fb77
ButongAnnie <ynzaballero.butong@gmail.com>
2023-11-10 15:21:55 +0800

    final pa sa final

--- src/inheritance/eggplant.java
+++ src/inheritance/eggplant.java
@@ -1,15 +1,13 @@
-
 package inheritance;
 
-
 public class eggplant extends vegetables {
-    
+
     public eggplant(String utan) {
         super(utan);
     }
-    
-        void daut(){
-        System.out.println(utan+" is lata man uy");
+
+    void daut() {
+        System.out.println(utan + " is lata man uy");
     }
-    
+
 }
